401685330a06342eaf05f9f0c29ef4bc3ecb7a96
Jose Vega <jvega48@calstatela.edu>
2017-09-28 20:11:52 -0700

    Sockets Project

---
+++ program_two/program_two/Source.c
@@ -0,0 +1,36 @@
+#include <sys/types.h>
+//unix h file
+//#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+	pid_t pid;
+	char *message;
+	int n;
+
+	printf("Fork program starting\n");
+	pid = fork();
+	switch (pid)
+	{
+	case -1:
+	perror:("fork failed");
+		exit(1);
+	case 0:
+		message = "this is the child";
+		n = 3;
+		break;
+	default:
+		message = "This is the parent";
+		n = 5;
+		break;
+	}
+
+	for (; n > 0; n--) {
+		puts(message);
+		sleep(1);
+	}
+	exit(0);
+}
